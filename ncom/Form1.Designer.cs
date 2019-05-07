bf4bff8b7173221e2a100f7f5dac82d69770c07c
Huayta <mauro.huayta@ecolab.com>
2019-05-07 00:38:53 -0300

    TP hasta el hito 2

---
+++ ncom/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ncom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butSuma = new System.Windows.Forms.Button();
+            this.butResta = new System.Windows.Forms.Button();
+            this.butMultiplic = new System.Windows.Forms.Button();
+            this.butDivision = new System.Windows.Forms.Button();
+            this.butPolar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.butBinomic = new System.Windows.Forms.Button();
+            this.lblPolar = new System.Windows.Forms.Label();
+            this.lblResul = new System.Windows.Forms.Label();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.txtEntrada2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // butSuma
+            // 
+            this.butSuma.Location = new System.Drawing.Point(43, 206);
+            this.butSuma.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.butSuma.Name = "butSuma";
+            this.butSuma.Size = new System.Drawing.Size(110, 60);
+            this.butSuma.TabIndex = 0;
+            this.butSuma.Text = "+";
+            this.butSuma.UseVisualStyleBackColor = true;
+            this.butSuma.Click += new System.EventHandler(this.ButSuma_Click);
+            // 
+            // butResta
+            // 
+            this.butResta.Location = new System.Drawing.Point(189, 206);
+            this.butResta.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.butResta.Name = "butResta";
+            this.butResta.Size = new System.Drawing.Size(114, 62);
+            this.butResta.TabIndex = 1;
+            this.butResta.Text = "-";
+            this.butResta.UseVisualStyleBackColor = true;
+            this.butResta.Click += new System.EventHandler(this.ButResta_Click);
+            // 
+            // butMultiplic
+            // 
+            this.butMultiplic.Location = new System.Drawing.Point(43, 296);
+            this.butMultiplic.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.butMultiplic.Name = "butMultiplic";
+            this.butMultiplic.Size = new System.Drawing.Size(110, 68);
+            this.butMultiplic.TabIndex = 2;
+            this.butMultiplic.Text = "X";
+            this.butMultiplic.UseVisualStyleBackColor = true;
+            this.butMultiplic.Click += new System.EventHandler(this.ButMultiplic_Click);
+            // 
+            // butDivision
+            // 
+            this.butDivision.Location = new System.Drawing.Point(189, 296);
+            this.butDivision.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.butDivision.Name = "butDivision";
+            this.butDivision.Size = new System.Drawing.Size(114, 71);
+            this.butDivision.TabIndex = 3;
+            this.butDivision.Text = "/";
+            this.butDivision.UseVisualStyleBackColor = true;
+            this.butDivision.Click += new System.EventHandler(this.ButDivision_Click);
+            // 
+            // butPolar
+            // 
+            this.butPolar.Location = new System.Drawing.Point(311, 54);
+            this.butPolar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.butPolar.Name = "butPolar";
+            this.butPolar.Size = new System.Drawing.Size(172, 55);
+            this.butPolar.TabIndex = 4;
+            this.butPolar.Text = "Pasar Num A a forma polar";
+            this.butPolar.UseVisualStyleBackColor = true;
+            this.butPolar.Click += new System.EventHandler(this.butPolar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Numero A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Numero B";
+            // 
+            // butBinomic
+            // 
+            this.butBinomic.Location = new System.Drawing.Point(501, 53);
+            this.butBinomic.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.butBinomic.Name = "butBinomic";
+            this.butBinomic.Size = new System.Drawing.Size(172, 56);
+            this.butBinomic.TabIndex = 10;
+            this.butBinomic.Text = "Pasar Num A a forma binomica";
+            this.butBinomic.UseVisualStyleBackColor = true;
+            this.butBinomic.Click += new System.EventHandler(this.ButBinomic_Click);
+            // 
+            // lblPolar
+            // 
+            this.lblPolar.AutoSize = true;
+            this.lblPolar.Location = new System.Drawing.Point(712, 71);
+            this.lblPolar.Name = "lblPolar";
+            this.lblPolar.Size = new System.Drawing.Size(134, 20);
+            this.lblPolar.TabIndex = 11;
+            this.lblPolar.Text = "Cambio de forma!";
+            // 
+            // lblResul
+            // 
+            this.lblResul.AutoSize = true;
+            this.lblResul.Location = new System.Drawing.Point(54, 425);
+            this.lblResul.Name = "lblResul";
+            this.lblResul.Size = new System.Drawing.Size(86, 20);
+            this.lblResul.TabIndex = 15;
+            this.lblResul.Text = "Resultado!";
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(130, 71);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(149, 26);
+            this.txtEntrada.TabIndex = 16;
+            // 
+            // txtEntrada2
+            // 
+            this.txtEntrada2.Location = new System.Drawing.Point(130, 122);
+            this.txtEntrada2.Name = "txtEntrada2";
+            this.txtEntrada2.Size = new System.Drawing.Size(149, 26);
+            this.txtEntrada2.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 880);
+            this.Controls.Add(this.txtEntrada2);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.lblResul);
+            this.Controls.Add(this.lblPolar);
+            this.Controls.Add(this.butBinomic);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.butPolar);
+            this.Controls.Add(this.butDivision);
+            this.Controls.Add(this.butMultiplic);
+            this.Controls.Add(this.butResta);
+            this.Controls.Add(this.butSuma);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butSuma;
+        private System.Windows.Forms.Button butResta;
+        private System.Windows.Forms.Button butMultiplic;
+        private System.Windows.Forms.Button butDivision;
+        private System.Windows.Forms.Button butPolar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button butBinomic;
+        private System.Windows.Forms.Label lblPolar;
+        private System.Windows.Forms.Label lblResul;
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.TextBox txtEntrada2;
+    }
+}
+
---
+++ ncom/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ncom
+{
+    public partial class Form1 : Form
+    {
+        string var1;
+        string var2;
+
+        string vara;
+        string varb;
+        
+        bool esBinomio;
+        bool esBinomio2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void butPolar_Click(object sender, EventArgs e)
+        {
+            
+            double M1 = obtenerModulo();
+            double ARG1 = obtenerArgumento();
+            if (esBinomio == true)
+            {
+
+                lblPolar.Text = "|Z1|:" + M1 + " ARG(Z1):" + ARG1;
+            }
+            else
+            {
+                lblPolar.Text = " El numero ya esta en forma polar!";
+            }
+              
+        }
+
+        private void ButBinomic_Click(object sender, EventArgs e)
+        {
+            if (esBinomio == false)
+            {
+
+                obtenerVariables1();
+                double R1 = Double.Parse(var1);
+                double R2 = Double.Parse(var2);
+                double af = Math.Round(obtenerReal(R1, R2), 3);
+                double bf = Math.Round(obtenerImg(R1, R2), 3);
+                if (bf >= 0)
+                {
+                    lblPolar.Text = af + " + " + bf + " i";
+                }
+                else
+                {
+                    lblPolar.Text = af + " - " + Math.Abs(bf) + " i";
+                }
+
+            }
+            else
+            {
+                lblPolar.Text = " El numero ya esta en forma binomica! ";
+            }
+
+
+
+        }
+     
+        public double obtenerModulo()
+        {
+            obtenerVariables1();
+          
+            double R1 = Double.Parse(var1);
+            double IM1 = Double.Parse(var2);
+
+
+
+            double M1 = Math.Sqrt(Math.Pow(R1, 2) + Math.Pow(IM1, 2));
+
+
+            return M1;
+        }
+        public double obtenerArgumento()
+        {
+            obtenerVariables1();
+            double R1 = Double.Parse(var1);
+            double IM1 = Double.Parse(var2);
+            double ARG1;
+            double complemento = Math.PI;
+            if (R1 >= 0)
+                {
+                //ARG1 = Math.Round(Math.Atan(IM1 / R1), 2);
+                ARG1 = Math.Atan(IM1 / R1);
+                }
+            else
+                {
+                //ARG1 = Math.Round((Math.Atan(IM1 / R1)) + complemento, 2);
+                ARG1 = (Math.Atan(IM1 / R1) + complemento);
+                }
+            return ARG1;
+        }       
+       
+
+        private void ButSuma_Click(object sender, EventArgs e)
+        {
+            obtenerVariables1();
+            obtenerVariables2();
+            if (esBinomio == true & esBinomio2 == true)
+            {
+
+
+                double R1 = Double.Parse(var1);
+                double IM1 = Double.Parse(var2);
+                double R2 = Double.Parse(vara);
+                double IM2 = Double.Parse(varb);
+                Double Resultado1 = Math.Round(R1 + R2, 4);
+                Double Resultado2 = Math.Round(IM1 + IM2, 4);
+
+                if (Resultado2 >= 0)
+                {
+                    lblResul.Text = Resultado1 + " + " + Resultado2 + " i";
+                }
+                else
+                {
+                    lblResul.Text = Resultado1 + " - " + Math.Abs(Resultado2) + " i";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese los numeros en el formato binomico para la suma", "Error en formato", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void ButResta_Click(object sender, EventArgs e)
+        {
+            obtenerVariables1();
+            obtenerVariables2();
+
+            if (esBinomio == true & esBinomio2)
+            {
+
+
+                double R1 = Double.Parse(var1);
+                double IM1 = Double.Parse(var2);
+                double R2 = Double.Parse(vara);
+                double IM2 = Double.Parse(varb);
+                Double Resultado1 = Math.Round(R1 - R2, 4);
+                Double Resultado2 = Math.Round(IM1 - IM2, 4);
+
+                if (Resultado2 >= 0)
+                {
+                    lblResul.Text = Resultado1 + " + " + Resultado2 + " i";
+                }
+                else
+                {
+                    lblResul.Text = Resultado1 + " - " + Math.Abs(Resultado2) + " i";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor ingrese los numeros en el formato binomico para la resta", "Error en formato", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void ButMultiplic_Click(object sender, EventArgs e)
+        {
+            obtenerVariables1();
+            obtenerVariables2();
+            if (esBinomio == true & esBinomio2 == true)
+            {
+                double R1 = Double.Parse(var1);
+                double IM1 = Double.Parse(var2);
+                double R2 = Double.Parse(vara);
+                double IM2 = Double.Parse(varb);
+                Double Resultado1 = Math.Round((R1 * R2) + (IM1 * IM2 * (-1)), 4);
+                Double Resultado2 = Math.Round((R1 * IM2) + (IM1 * R2), 4);
+
+                if (Resultado2 >= 0)
+                {
+                    lblResul.Text = Resultado1 + " + " + Resultado2 + " i";
+                }
+                else
+                {
+                    lblResul.Text = Resultado1 + " - " + Math.Abs(Resultado2) + " i";
+                }
+            }
+            else
+            {
+                if (esBinomio == false & esBinomio2 == false)
+                {
+                    double A = Double.Parse(var1);
+                    double B = Double.Parse(var2);
+                    double A2 = Double.Parse(vara);
+                    double B2 = Double.Parse(varb);
+                    Double Resultado1 = Math.Round((A * A2), 4);
+                    Double Resultado2 = Math.Round(B + B2, 4);
+                    lblResul.Text = "[ " + Resultado1 + " ;" + Resultado2 + " ]";
+
+                }
+                else
+                {
+                    MessageBox.Show("Por favor ingrese los numeros en el mismo formato para la multiplicación", "Error en formato", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+
+        private void ButDivision_Click(object sender, EventArgs e)
+        {
+            obtenerVariables1();
+            obtenerVariables2();
+            if (esBinomio == true & esBinomio2 == true)
+            {
+                
+                double R1 = Double.Parse(var1);
+                double IM1 = Double.Parse(var2);
+                double R2 = Double.Parse(vara);
+                double IM2 = Double.Parse(varb);
+                Double numreal1 = Math.Round((R1 * R2) + (IM1 * IM2), 4);
+                Double numim2 = Math.Round((R2 * IM1) - (R1 * IM2), 4);
+                Double denominador = Math.Round(Math.Pow(R2, 2) + Math.Pow(IM2, 2), 4);
+                String partereal = numreal1 + " / " + denominador;
+                String parteim = numim2 + " / " + denominador;
+
+                if (denominador == 0)
+                {
+
+                    lblResul.Text = "Denominador es 0, no valido ";
+
+                }
+                else
+                {
+                    lblResul.Text = "(" + partereal + ")" + " + " + "(" + parteim + ")" + " i ";
+                }
+            }
+            else
+            {
+                if (esBinomio2==false & esBinomio==false)
+                {
+                    double A = Double.Parse(var1);
+                    double B = Double.Parse(var2);
+                    double A2 = Double.Parse(vara);
+                    double B2 = Double.Parse(varb);
+                    Double Resultado1 = Math.Round((A / A2), 4);
+                    Double Resultado2 = Math.Round(B - B2, 4);
+                    lblResul.Text = "[ " + Resultado1 + " ;" + Resultado2 + " ]";
+                }
+                else
+                {
+                    MessageBox.Show("Por favor ingrese los numeros en el mismo formato para la division", "Error en formato", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+
+        }
+        public void obtenerVariables1()
+        {
+            string entrada = txtEntrada.Text;
+            int largo = entrada.Length - 1;
+            if (entrada[0] == '(' & entrada[largo] == ')')
+            {
+                esBinomio = true;
+                entrada = entrada.TrimStart('(').TrimEnd(')');
+
+            }
+            else
+            {
+                if (entrada[0] == '[' & entrada[largo] == ']')
+                {
+                    esBinomio = false;
+                    entrada = entrada.TrimStart('[').TrimEnd(']');
+                }
+                else
+                {
+                   MessageBox.Show("Por favor ingrese el numero en el formato", "Error en formato", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                 
+            }
+
+            string[] entradasplit = entrada.Split(';');
+            var1 = entradasplit[0];
+            var2 = entradasplit[1];
+
+        }
+
+        public void obtenerVariables2()
+        {
+            string entrada2 = txtEntrada2.Text;
+            int largo = entrada2.Length - 1;
+            if (entrada2[0] == '(' & entrada2[largo] == ')')
+            {
+                esBinomio2 = true;
+                entrada2 = entrada2.TrimStart('(').TrimEnd(')');
+
+            }
+            else
+            {
+                if (entrada2[0] == '[' & entrada2[largo] == ']')
+                {
+                    esBinomio2 = false;
+                    entrada2 = entrada2.TrimStart('[').TrimEnd(']');
+                }
+                else
+                {
+                   MessageBox.Show("Por favor ingrese el numero en el formato", "Error en formato", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                 
+            }
+
+            string[] entradasplit2 = entrada2.Split(';');
+            vara = entradasplit2[0];
+            varb = entradasplit2[1];
+
+        }
+        public double obtenerReal(double a, double b)
+        {
+            double R = a * (Math.Cos(b));
+            return R;
+        }
+        public double obtenerImg(double a, double b)
+        {
+            double I = a * (Math.Sin(b));
+            return I;
+        }
+
+     
+    }
+}
